c928da15afd485ed70f8d6964f136635783d62e7
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-02-17 11:17:50 -0500

    Remove unused methods from Boot and Menu game states

--- src/states/Boot.ts
+++ src/states/Boot.ts
@@ -1,10 +1,6 @@
 import { baseHeight, baseWidth, scale } from '../constants';
 
 export class Boot extends Phaser.State {
-  public preload() {
-
-  }
-
   public create() {
     this.stage.smoothed = false;
     this.scale.minWidth = baseWidth;
--- src/states/Menu.ts
+++ src/states/Menu.ts
@@ -1,13 +1,5 @@
 export class Menu extends Phaser.State {
-  public preload() {
-
-  }
-
   public create() {
     this.state.start('Game');
-  }
-
-  public update() {
-
   }
 }
