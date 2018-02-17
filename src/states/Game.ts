299081e88830fef8a4b118fdfc416bca8066295f
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-02-17 11:21:41 -0500

    Add map and player to Game game state

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,13 +1,24 @@
-export class Game extends Phaser.State {
-  public preload() {
+import { Player } from '../prefabs/Player';
 
-  }
+export class Game extends Phaser.State {
+  private layer: Phaser.TilemapLayer;
+  private map: Phaser.Tilemap;
+  private player: Player;
 
   public create() {
-    console.log('Hello world!');
+    this.map = this.add.tilemap('walls', 64, 64);
+    this.map.addTilesetImage('tileset', 'tileset');
+    this.map.setCollision(1);
+    this.layer = this.map.createLayer('Tile Layer 1');
+    this.layer.resizeWorld();
+
+    this.player = new Player(this.game, 192, 192);
+    this.add.existing(this.player);
+    this.camera.follow(this.player);
   }
 
   public update() {
-
+    this.player.update();
+    this.game.physics.arcade.collide(this.player, this.layer);
   }
 }
