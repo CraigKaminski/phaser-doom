4b6d66e1f0c3a2da52bdde043a1bf3c24f8f459c
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-02-17 11:17:13 -0500

    Add player and map assets

--- src/states/Preloader.ts
+++ src/states/Preloader.ts
@@ -1,6 +1,8 @@
 export class Preloader extends Phaser.State {
   public preload() {
-
+    this.load.image('player', 'images/player.png');
+    this.load.tilemap('walls', 'levels/map.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('tileset', 'images/tileset.png');
   }
 
   public create() {
